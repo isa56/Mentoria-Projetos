8d62d09baf113204e18566cc20efb53b762f1163
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2020-11-21 14:23:21 -0300

    Adicionado projeto do recyclerview

---
+++ RecyclerAndNotification/app/src/main/java/com/meteoro/recyclerandnotificationexample/MainActivity.java
@@ -0,0 +1,29 @@
+package com.meteoro.recyclerandnotificationexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnVertical;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnVertical = findViewById(R.id.btn_vertical);
+
+        btnVertical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RecyclerVerticalActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
